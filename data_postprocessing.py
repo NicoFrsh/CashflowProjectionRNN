Fix MSE and MAE calculation

data_postprocessing.py
@@ -6,9 +6,12 @@ def recursive_prediction(test_data):
     
     return True
 
-# Function to compute the average of the output value (over all scenarios) for all timesteps.
 def calculate_mean_per_timestep(outputs, timesteps):
-    
+    """
+    Calculates the mean output value for all timesteps over all scenarios.
+    Returns an array of length <timesteps> containing the mean output value for each timestep.
+    """
+
     mean_outputs = []
 
 
@@ -26,7 +29,44 @@ def calculate_mean_per_timestep(outputs, timesteps):
 
     return mean_outputs
 
+def calculate_loss_per_timestep(targets, predictions, timesteps = 59, loss_metric = 'mse'):
+    """ 
+    Calculates the loss per timestep over all scenarios. You can choose between Mean-Squared-Error ('mse')
+    and Mean-Absolute-Error ('mae') as loss metric.
+    Returns an array of length <projection_time> (we start predicting at timestep 1)
+    containing the MSE for each timestep.
+    """
+
+    loss = []
+
+    for i in range(timesteps):
+
+        # Extract targets and predictions for each scenario at timestep i
+        targets_extract = targets[i::timesteps].copy()
+        predictions_extract = predictions[i::timesteps].copy()
+
+        assert (len(targets_extract) == len(targets)/timesteps and 
+        len(predictions_extract) == len(predictions)/timesteps), 'Step size is not aligned with output length!'            
+
+        if (loss_metric == 'mse'):
+            # Compute MSE at timestep i
+            loss_i = (np.square(targets_extract - predictions_extract)).mean()
+
+        elif (loss_metric == 'mae'):
+            # Compute MAE at timestep i
+            loss_i = (np.abs(targets_extract - predictions_extract)).mean()
+
+        loss.append(loss_i)
+            
+
+    loss = np.array(loss)
+    return loss
+
 def calculate_mean_per_scenario(outputs, timesteps):
+    """
+    Calculates the mean output value for each scenario over all time steps.
+    Returns an array of length <number_scenarios> containing the mean output value for each scenario.
+    """
 
     number_scenarios = int(len(outputs)/timesteps)
     mean_outputs = []
@@ -45,56 +85,68 @@ def calculate_mean_per_scenario(outputs, timesteps):
 
     return mean_outputs
 
-def calculate_loss_per_timestep(targets, predictions, loss_metric = 'mse'):
-    """ 
-    Calculates the loss per timestep over all scenarios. You can chosse between Mean-Squared-Error ('mse')
-    and Mean-Absolute-Error ('mae') as loss metric.
-    Returns an array of length <projection_time - 1> (we start predicting at timestep 1)
-    containing the MSE for each timestep.
+def calculate_loss_per_scenario(targets, predictions, timesteps = 59, loss_metric = 'mse'):
     """
-    targets_mean = calculate_mean_per_timestep(targets, 59)
-    predictions_mean = calculate_mean_per_timestep(predictions, 59)
-    number_scenarios = len(targets) / 59
-
-    loss = []
-
-    for i in range(len(targets_mean)):
-
-        if loss_metric == 'mse':
-            current_loss = (targets_mean[i] - predictions_mean[i])**2 
-            current_loss = current_loss / number_scenarios
-
-        elif loss_metric == 'mae':
-            current_loss = abs(targets_mean[i] - predictions_mean[i])
-            current_loss = current_loss / number_scenarios
-        
-        loss.append(current_loss)
-
-    return loss
-
-def calculate_loss_per_scenario(targets, predictions, loss_metric = 'mse'):
-    """
-    Calculates the mean-squared-error per scenario over all timesteps. You can chosse between Mean-Squared-Error ('mse')
+    Calculates the mean-squared-error per scenario over all timesteps. You can choose between Mean-Squared-Error ('mse')
     and Mean-Absolute-Error ('mae') as loss metric.
     Returns an array of length <number_scenarios> containing the MSE for each scenario.
     """
-    targets_mean = calculate_mean_per_scenario(targets, 59)
-    predictions_mean = calculate_mean_per_scenario(predictions, 59)
-    # TODO: Problem: Anzahl Szenarien ist nicht immer 5001!! Training: 5001*0.8 z.b.
-    number_timesteps = 59
 
     loss = []
 
-    for i in range(len(targets_mean)):
+    number_scenarios = int(len(targets)/timesteps)
 
-        if loss_metric == 'mse':
-            current_loss = (targets_mean[i] - predictions_mean[i])**2
-            current_loss = current_loss / number_timesteps
+    for i in range(number_scenarios):
 
-        elif loss_metric == 'mae':
-            current_loss = abs(targets_mean[i] - predictions_mean[i])
-            current_loss = current_loss / number_timesteps
-        
-        loss.append(current_loss)
+        targets_extract = targets[i * timesteps : (i+1) * timesteps]
+        predictions_extract = predictions[i * timesteps : (i+1) * timesteps]
 
+        assert (len(targets_extract) == timesteps and
+        len(predictions_extract) == timesteps), 'Step size is not aligned with output length!'
+
+        if (loss_metric == 'mse'):
+            # Compute MSE of i-th scenario
+            loss_i = (np.square(targets_extract - predictions_extract)).mean()
+
+        elif (loss_metric == 'mae'):
+            # Compute MAE of i-th scenario
+            loss_i = (np.abs(targets_extract - predictions_extract)).mean()
+
+        loss.append(loss_i)
+
+    loss = np.array(loss)
+    # Check:
+    total_loss = np.mean(loss)
+    print('total_loss (per scenario): ', total_loss)
     return loss
+
+# def calculate_loss_per_scenario(targets, predictions, loss_metric = 'mse'):
+#     """
+#     Calculates the mean-squared-error per scenario over all timesteps. You can choose between Mean-Squared-Error ('mse')
+#     and Mean-Absolute-Error ('mae') as loss metric.
+#     Returns an array of length <number_scenarios> containing the MSE for each scenario.
+#     """
+#     targets_mean = calculate_mean_per_scenario(targets, 59)
+#     predictions_mean = calculate_mean_per_scenario(predictions, 59)
+#     # TODO: Problem: Anzahl Szenarien ist nicht immer 5001!! Training: 5001*0.8 z.b.
+#     number_timesteps = 59
+
+#     loss = []
+
+#     for i in range(len(targets_mean)):
+
+#         if loss_metric == 'mse':
+#             current_loss = (targets_mean[i] - predictions_mean[i])#**2
+#             # current_loss = current_loss / number_timesteps
+
+#         elif loss_metric == 'mae':
+#             current_loss = abs(targets_mean[i] - predictions_mean[i])
+#             # current_loss = current_loss / number_timesteps
+        
+#         loss.append(current_loss)
+
+#     # Check if total loss is same as the score returned by evaluate function
+#     total_loss = np.sum(np.array(loss))
+#     print('total loss (per scenario), {}: '.format(loss_metric), total_loss)
+
+#     return loss