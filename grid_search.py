864b32c78a7a6f368e4025fcefeed12981b5ee6f
NicoFrsh <frischnico@web.de>
2022-05-26 10:11:27 +0200

    prepare grid search

--- grid_search.py
+++ grid_search.py
@@ -3,7 +3,9 @@ import numpy as np
 from tensorflow import keras
 import keras_tuner
 
-hp = keras_tuner.HyperParameters()
-model_type = hp.Choice('model_type', ['simple_rnn', 'lstm', 'gru'])
-learning_rate = hp.Float('learning_rate', ['0.01','0.005', '0.001'])
+from model import RNN_Model
+
+def build_model(hp):
+    model_type = hp.Choice('model_type', ['simple_rnn', 'lstm', 'gru'])
+    learning_rate = hp.Float('learning_rate', ['0.01','0.005', '0.001'])
 
